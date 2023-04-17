5bb91b692bebe1b376406ecda1a22d8138ce03e1
Cristiano Ferreira da Silva <toshiekuri1977@gmail.com>
2023-04-18 04:43:35 +0900

    tentando subir GitHub

--- Set.js
+++ Set.js
@@ -6,4 +6,5 @@ const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
 
 const nomesAtualizados = [...new Set(nomes)];  // Escrevendo dessa maneira, a linha 5 ficaria como a linha 7, eliminando a linha 3, e ficando mais compacto.
 
+
 console.log(nomesAtualizados);
